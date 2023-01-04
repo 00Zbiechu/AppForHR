f9e6bd6bed568e3d008fb2eda214e9a234fdb62b
Technik Informatyk <00Zbiewski@gmail.com>
2023-01-04 18:48:18 +0100

    Init Repo

---
+++ src/main/java/pl/hrmanagement/appforhr/AppforhrApplication.java
@@ -0,0 +1,13 @@
+package pl.hrmanagement.appforhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppforhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppforhrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/hrmanagement/appforhr/AppforhrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hrmanagement.appforhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppforhrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
