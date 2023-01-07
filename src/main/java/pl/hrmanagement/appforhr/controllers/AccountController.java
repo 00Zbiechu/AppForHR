024a88dd8310099b2ade7431ea3aa277e53ed91a
Technik Informatyk <00Zbiewski@gmail.com>
2023-01-07 02:08:39 +0100

    Walidacja formularza rejestracji - bez komunikatów o błędnym wypełnieniu formularza

--- src/main/java/pl/hrmanagement/appforhr/controllers/AccountController.java
+++ src/main/java/pl/hrmanagement/appforhr/controllers/AccountController.java
@@ -3,34 +3,41 @@ package pl.hrmanagement.appforhr.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import pl.hrmanagement.appforhr.entity.Account;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.hrmanagement.appforhr.dto.AccountDto;
 import pl.hrmanagement.appforhr.service.AccountService;
 
-
+import javax.validation.Valid;
 
 
 @Controller
 @RequiredArgsConstructor
-public class AccountController{
+public class AccountController {
 
     private final AccountService accountService;
 
+
     @GetMapping("/register")
     public String getRegisterPage(Model model) {
-        model.addAttribute("newAccount",new Account());
+        model.addAttribute("newAccount", new AccountDto());
         return "register";
     }
 
     @PostMapping("/add-account")
-    public String addAccount(@ModelAttribute Account account){
+    public String addAccount(@Valid AccountDto accountDto,BindingResult result, Model model) {
 
-            accountService.saveAccount(account);
-            return "login";
+        if (result.hasErrors()) {
+            model.addAttribute("newAccount", new AccountDto());
+            return "register";
+        }
+
+            accountService.saveAccount(accountDto);
+            return "redirect:/login";
+
 
     }
+
 
 
     @GetMapping("/app")
